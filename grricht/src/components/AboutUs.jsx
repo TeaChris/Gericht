2e2c63e8acd888a23ea64c844f352de51b62a4f0
bermuda <teechris110@gmail.com>
2023-04-23 22:00:41 +0100

    about

--- grricht/src/components/AboutUs.jsx
+++ grricht/src/components/AboutUs.jsx
@@ -1,8 +1,8 @@
 const AboutUs = () => {
   return (
-    <div className="">
+    <section className="flex items-center gap-4 bg-background w-full h-[40rem]">
       <h2 className=" text-black text-3xl">AboutUs</h2>
-    </div>
+    </section>
   )
 }
 
